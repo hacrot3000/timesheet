0d0541e7716b890446306560602731135ba33526
Trương Chương Dương <a0a1a2@gmail.com>
2023-04-12 17:59:05 +0700

    Update get_users.py

--- app/ThirdParty/get_users.py
+++ app/ThirdParty/get_users.py
@@ -27,7 +27,7 @@ sql = "INSERT IGNORE INTO users (id, username, fullname, is_admin, paid_leave_pe
 
 
 conn = None
-zk = ZK('192.168.1.14', port=4370)
+zk = ZK('<IP>', port=4370)
 try:
     conn = zk.connect()
     print ('Disabling device ...')
